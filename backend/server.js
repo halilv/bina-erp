7b37d41cf3e5a4172a7cbe2d904ce642870611cc
halilv <hveznedaroglu@gmail.com>
2026-06-26 12:07:15 +0300

    CORS duzeltildi

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors({
+app.use(cors());
   origin: '*',
     'http://localhost:3001',
     'http://localhost:3002',
